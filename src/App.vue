c4f92f198c4c70f5917a144c29c25d14a7672fc2
Javier Pestana <javier.pestana85@gmail.com>
2024-09-10 16:04:15 +0100

    update guardarPAciente function to handle edition too

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, reactive } from "vue";
+import { uid } from "uid";
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
 import Paciente from "./components/Paciente.vue";
@@ -7,6 +8,7 @@ import Paciente from "./components/Paciente.vue";
 const pacientes = ref([]);
 
 const paciente = reactive({
+  id: null,
   nombre: "",
   propietario: "",
   email: "",
@@ -15,7 +17,15 @@ const paciente = reactive({
 });
 
 const guardarPaciente = () => {
-  pacientes.value.push({ ...paciente });
+  if (paciente.id) {
+    const { id } = paciente;
+    const i = pacientes.value.findIndex(
+      (pacienteState) => pacienteState.id === id
+    );
+    pacientes.value[i] = { ...paciente };
+  } else {
+    pacientes.value.push({ ...paciente, id: uid() });
+  }
 
   Object.assign(paciente, {
     nombre: "",
@@ -23,7 +33,15 @@ const guardarPaciente = () => {
     email: "",
     alta: "",
     sintomas: "",
+    id: null,
   });
+};
+
+const actualizarPaciente = (id) => {
+  const pacienteEditar = pacientes.value.filter(
+    (paciente) => paciente.id === id
+  )[0];
+  Object.assign(paciente, pacienteEditar);
 };
 </script>
 
@@ -48,7 +66,11 @@ const guardarPaciente = () => {
             Información de
             <span class="text-indigo-600 font-bold">Pacientes</span>
           </p>
-          <Paciente v-for="paciente in pacientes" :paciente="paciente" />
+          <Paciente
+            v-for="paciente in pacientes"
+            :paciente="paciente"
+            @actualizar-paciente="actualizarPaciente"
+          />
         </div>
         <p v-else class="mt-20 text-2xl text-center">No Hay Pacientes</p>
       </div>
--- src/components/Paciente.vue
+++ src/components/Paciente.vue
@@ -1,4 +1,6 @@
 <script setup>
+defineEmits(["actualizar-paciente"]);
+
 defineProps({
   paciente: {
     type: Object,
@@ -11,7 +13,7 @@ defineProps({
   <div class="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
     <p class="font-bold mb-3 text-gray-700 uppercase">
       ID:
-      <span class="font-normal normal-case"> </span>
+      <span class="font-normal normal-case"> {{ paciente.id }} </span>
     </p>
 
     <p class="font-bold mb-3 text-gray-700 uppercase">
@@ -43,6 +45,7 @@ defineProps({
       <button
         type="button"
         class="block w-full py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        @click="$emit('actualizar-paciente', paciente.id)"
       >
         Editar
       </button>
